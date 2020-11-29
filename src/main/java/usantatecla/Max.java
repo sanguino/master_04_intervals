8934a29560486dc28ff2d8e03a872edb7471b7e7
Miguel G. Sanguino <sanguino@me.com>
2020-11-29 01:14:44 +0100

    move hashCode and equals to Boundary

--- src/main/java/usantatecla/Max.java
+++ src/main/java/usantatecla/Max.java
@@ -11,30 +11,6 @@ public class Max extends Boundary {
   }
 
   @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    long temp;
-    temp = Double.doubleToLongBits(value);
-    result = prime * result + (int) (temp ^ (temp >>> 32));
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    Max other = (Max) obj;
-    if (Double.doubleToLongBits(value) != Double.doubleToLongBits(other.value))
-      return false;
-    return true;
-  }
-
-  @Override
   public String toString() {
     return this.value + ")";
   }
