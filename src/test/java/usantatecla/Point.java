1bf1004d59ae22d959e2c0b153f2c9e58c61e289
Miguel G. Sanguino <sanguino@me.com>
2020-11-29 03:45:19 +0100

    green, remove getVery from point

--- src/test/java/usantatecla/Point.java
+++ src/test/java/usantatecla/Point.java
@@ -3,7 +3,6 @@ package usantatecla;
 public class Point {
 
   static final double DEVIATION = 0.1;
-  static final double VERY_DEVIATION = 1;
   final double value;
 
   Point(double value){
@@ -14,20 +13,12 @@ public class Point {
     return this.value - Point.DEVIATION;
   }
 
-  double getVeryLess() {
-    return this.value - Point.VERY_DEVIATION;
-  }
-
   double getEquals() {
     return this.value;
   }
 
   double getGreater() {
     return this.value + Point.DEVIATION;
-  }
-
-  double getVeryGreater() {
-    return this.value + Point.VERY_DEVIATION;
   }
   
 }
