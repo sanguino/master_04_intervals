23d64dfb540f939ad6291956d28193cfa4b5b07c
Miguel G. Sanguino <sanguino@me.com>
2020-11-29 01:38:00 +0100

    red, add very methods to Point

--- src/test/java/usantatecla/Point.java
+++ src/test/java/usantatecla/Point.java
@@ -3,6 +3,7 @@ package usantatecla;
 public class Point {
 
   static final double DEVIATION = 0.1;
+  static final double VERY_DEVIATION = 1;
   final double value;
 
   Point(double value){
@@ -13,12 +14,20 @@ public class Point {
     return this.value - Point.DEVIATION;
   }
 
+  double getVeryLess() {
+    return this.value - Point.VERY_DEVIATION;
+  }
+
   double getEquals() {
     return this.value;
   }
 
   double getGreater() {
     return this.value + Point.DEVIATION;
+  }
+
+  double getVeryGreater() {
+    return this.value + Point.VERY_DEVIATION;
   }
   
 }
